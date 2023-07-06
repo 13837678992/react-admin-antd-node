d5aade00778b5d9d1a90de18a01a0f7ff40abce5
weicheng <1678264008@qq.com>
2023-07-06 15:18:46 +0800

    feat:公共方法

--- react-admin/src/utils/index.ts
+++ react-admin/src/utils/index.ts
@@ -1,2 +1,29 @@
 
+// 获取storage的大小
+function getStorageSize(storage: Storage): number {
+    let totalSize = 0;
+
+    for (let i = 0; i < storage.length; i++) {
+        const key: string | null = storage.key(i);
+        if (key) {
+            const value: string | null = storage.getItem(key);
+            if (value) {
+                totalSize += key.length + value.length;
+            }
+        }
+    }
+
+    return totalSize;
+}
+
+// 获取 localStorage 的大小
+const localStorageSize:number = getStorageSize(localStorage);
+// 获取 sessionStorage 的大小
+const sessionStorageSize:number = getStorageSize(sessionStorage);
+
 export {default as request} from './request'
+export {
+    getStorageSize,
+    localStorageSize,
+    sessionStorageSize
+}
